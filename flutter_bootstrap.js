dc501a4b2951d356e8d8c795d16d6269cde6e8be
Chantikaputrii <24111814037@mhs.unesa.ac.id>
2025-10-08 09:20:56 +0700

    fix base href for github pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "988804730"
+    serviceWorkerVersion: "2746136995"
   }
 });
